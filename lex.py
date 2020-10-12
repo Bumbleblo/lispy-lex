0be7cdcff6d7d8bc2cca6a84b7d515ff574f68aa
Felipe Borges <bumbleblo2013@gmail.com>
2020-10-12 20:20:52 -0300

    Initial lexer
    Signed-off-by: Felipe Borges <bumbleblo2013@gmail.com>

--- lex.py
+++ lex.py
@@ -6,6 +6,8 @@ class Token(NamedTuple):
     kind: str
     value: str
 
+r"(?P<token>[\(\)])|(?P<symbol>[a-zA-Z]\w*)|(?P<number>\d+(?:\.\d+)?)"
+
 
 def lex(code: str) -> Iterable[Token]:
     """
@@ -13,4 +15,43 @@ def lex(code: str) -> Iterable[Token]:
     da string de código fornecida.
     """
 
-    return [Token('INVALIDA', 'valor inválido')]
+    # symbol : regex
+    patterns = {
+        'n-comentario': ';;.*$',
+        'LPAR': r'\(',
+        'RPAR': r'\)',
+        'STRING': r'\".*\"',
+        'BOOL': '#[tf]',
+        'CHAR': '#\\\\\w+',
+        'NUMBER': r'[+-]?\d+(?:\.\d+)?',
+        'NAME': r"\'|\".*\"|\.{3}|[a-zA-Z\.\%][\w\-\?\>!]*|[\/+-]",
+    }
+
+    print(f"Code: {code}")
+
+    pattern = ''
+    for key, value in patterns.items():
+
+        if re.match('n\-\w+', key):
+            pattern += f'(?:{value})|'
+        else:
+            pattern += f'(?P<{key}>{value})|'
+
+    pattern = pattern[:-1]
+
+    print(pattern)
+
+    pattern = re.compile(pattern)
+
+    result = [ ]
+
+    for each in pattern.finditer(code):
+
+        if each.lastgroup != None:
+            result.append(Token(each.lastgroup, each.group()))
+        #print(each.lastgroup)
+        #print(each)
+
+    print(result)
+    return result
+    #return [Token('INVALIDA', 'valor inválido')]
